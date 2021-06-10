e56cc7adae2c5df2bbac0e3e25813d6acc52a644
neeraj-pal-au19 <85250424+neeraj-pal-au19@users.noreply.github.com>
2021-06-10 23:43:19 +0530

    cc
    some file uploading

---
+++ user input.py
@@ -0,0 +1,10 @@
+a, b, c = 5, 7, 10
+ 
+if(a <= b and a <= c):
+    print(a, "is the smallest")
+ 
+elif(b <= a and b <= c):
+    print(b, "is the smallest")
+else:
+    print(c, "is the smallest")
+	
